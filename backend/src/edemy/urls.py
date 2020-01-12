a7955d4736d018734b1daebc12a18b61572511b2
AidenTran15 <aidenkiettran@gmail.com>
2020-01-13 00:21:19 +1100

    [New] Create User

--- backend/src/edemy/urls.py
+++ backend/src/edemy/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    url('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls)
 ]
