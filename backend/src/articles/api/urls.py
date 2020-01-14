923a34a1ee8fc8ae31ccf1d05284871afe499edd
AidenTran15 <aidenkiettran@gmail.com>
2020-01-14 23:39:18 +1100

    [New]Create urls for Api

---
+++ backend/src/articles/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import ArticleListView, ArticleDetailView
+
+urlpatterns = [
+    path('', ArticleDetailView.as_view())
+    path('pk', ArticleDetailView.as_view()),
+]
