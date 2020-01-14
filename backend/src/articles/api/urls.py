41fb3f763141d8f1b53ba0ccb69de1b9f9947a14
AidenTran15 <aidenkiettran@gmail.com>
2020-01-15 00:08:02 +1100

    [New] Testing backend

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('', ArticleDetailView.as_view())
-    path('pk', ArticleDetailView.as_view()),
+    path('', ArticleDetailView.as_view()),
+    path('<pk>', ArticleDetailView.as_view())
 ]
--- backend/src/edemy/urls.py
+++ backend/src/edemy/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api/', include('articles.api.urls'))
 ]
