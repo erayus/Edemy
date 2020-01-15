31d7b543e33be3a277dba1b9b7b39923e4f0447e
AidenTran15 <aidenkiettran@gmail.com>
2020-01-16 10:23:19 +1100

    [New] Added Update and Delete article

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -3,12 +3,17 @@ from django.urls import path
 from .views import (
     ArticleListView,
     ArticleDetailView,
-    ArticleCreateView
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView
 )
 
 
 urlpatterns = [
     path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
     path('create/', ArticleCreateView.as_view()),
+    path('<pk>', ArticleDetailView.as_view()),
+    path('<pk>/update', ArticleDetailView.as_view()),
+    path('<pk>delete', ArticleDetailView.as_view()),
+
 ]
