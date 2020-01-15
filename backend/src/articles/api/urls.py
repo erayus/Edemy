efdceffdff1547530fa74feeda3b94e9a1cc6857
AidenTran15 <aidenkiettran@gmail.com>
2020-01-16 10:26:26 +1100

    Testing Update

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -5,7 +5,7 @@ from .views import (
     ArticleDetailView,
     ArticleCreateView,
     ArticleUpdateView,
-    ArticleDeleteView
+    ArticleDeleteView,
 )
 
 
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', ArticleListView.as_view()),
     path('create/', ArticleCreateView.as_view()),
     path('<pk>', ArticleDetailView.as_view()),
-    path('<pk>/update', ArticleDetailView.as_view()),
-    path('<pk>delete', ArticleDetailView.as_view()),
+    path('<pk>/update/', ArticleUpdateView.as_view()),
+    path('<pk>delete/', ArticleDeleteView.as_view()),
 
 ]
