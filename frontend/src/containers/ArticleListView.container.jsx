c98065c2795d3b77c4a4fd1eb0f47e0348e772dc
Erayus <raymondhieutran@gmail.com>
2020-01-19 10:50:20 +1100

    [New] Adding Article and ArticleListView

---
+++ frontend/src/containers/ArticleListView.container.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Article from '../components/Article.component'
+class ArticleList extends Component { 
+    render() {
+        return (
+            <div>
+                <Article></Article>
+            </div>
+        );
+    }
+}
+
+
+export default ArticleList;
