43ae42a1ddb17edf34e8663007d040c01e257e3d
AidenTran15 <aidenkiettran@gmail.com>
2020-01-22 23:50:20 +1100

    Testing backend

--- backend/src/edemy/urls.py
+++ backend/src/edemy/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('articles.api.urls'))
+    path('api/', include('articles.api.urls')),
 ]
